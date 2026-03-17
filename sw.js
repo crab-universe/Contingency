4a883df4100bcb5617e0e60eaf2fc197237bea2f
crab-universe <apraino3@gmail.com>
2026-03-16 21:21:45 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'Contingency';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
+});
